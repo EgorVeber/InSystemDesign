5687412a04a530bbc3bb48bf427b6480ee39f343
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-12-15 22:30:20 +0300

    Привер вроде рабочи screen wight указывает на самом lеле configuration.screenWidthDp

--- app/src/main/java/ru/gb/veber/insystemdesign/MainActivity.kt
+++ app/src/main/java/ru/gb/veber/insystemdesign/MainActivity.kt
@@ -1,11 +1,36 @@
 package ru.gb.veber.insystemdesign
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import ru.gb.veber.insystemdesign.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val width = displayMetrics.widthPixels
+
+        Log.d("TAG", "width = $width")
+
+        val configuration: Configuration = this.resources.configuration
+        //Текущая ширина доступного пространства экрана в единицах dp, соответствующая квалификатору ресурса ширины экрана.
+        val screenWidthDp: Int = configuration.screenWidthDp
+
+        //Наименьший размер экрана, который приложение увидит при нормальной работе, соответствующий квалификатору ресурса наименьшей ширины экрана.
+        val smallestScreenWidthDp: Int = configuration.smallestScreenWidthDp
+
+        Log.d("TAG", "screenWidthDp = $screenWidthDp")
+        Log.d("TAG", "smallestScreenWidthDp = $smallestScreenWidthDp")
     }
 }
